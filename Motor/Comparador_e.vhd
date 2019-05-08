d5ff37a8b323c3d8bda03db9202af59f5a6dde17
Caio Telles <cast@icomp.ufam.edu.br>
2019-05-08 14:25:24 -0400

    Correção
    Correção do diagrama em blocos e inserção do bloco de sincronismo

--- Motor/Comparador_e.vhd
+++ Motor/Comparador_e.vhd
@@ -4,8 +4,8 @@ use ieee.std_logic_1164.all;
 entity Comparador_e is
 	port(
 			en	         : in std_logic;
-			a				: in std_logic;
-			b 				: in std_logic_vector(18 downto 0);
+			a				: in std_logic_vector(6 downto 0);
+			b 				: in std_logic_vector(6 downto 0);
 			ls				: out std_logic
 		 );
 end Comparador_e;
---
+++ Motor/One_delay.vhd
@@ -0,0 +1,15 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity One_delay is
+  port (
+    d  : in  std_logic;
+    q  : out std_logic;
+    clk   : in  std_logic
+  );
+end One_delay;
+
+architecture onedelay_arc of One_delay is
+begin
+
+end onedelay_arc;
