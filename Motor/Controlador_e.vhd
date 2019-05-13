9c83656b37916e2a5586a27c0416f9005f524669
Caio Telles <cast@icomp.ufam.edu.br>
2019-05-13 14:41:09 -0400

    Finalização da máquina de estados do controlador

--- Motor/Controlador_e.vhd
+++ Motor/Controlador_e.vhd
@@ -15,20 +15,45 @@ entity Controlador_e is
 end Controlador_e;
 
 architecture arq1 of Controlador_e is
-	TYPE State_type IS (off_controller, high_on, high_off, low_on, low_off);
+	TYPE State_type IS (off_controller, ed_local, inc_local, inc_remote, set_remote);
 	signal state_controller : State_type;
 
-	begin
-
---	begin
---		process(clk,en4)
---		begin
---			if en4 = '0' then
---				state_controller <= off;
---			elsif (clk'event and clk = '0') then
---				case state_controller IS
---					
---					when off =>
---						if sp_control = '1'
---	
+begin
+		process(clk,en4)
+		begin
+			if en4 = '0' then
+				state_controller <= off_controller;
+			elsif (clk'event and clk = '0') then
+				case state_controller IS
+					
+					when off_controller =>
+						if sp_control = '1' and rt_control = '1' then
+							state_controller <= inc_local;
+						elsif sp_control = '0' and  rt_control = '0' then
+							state_controller <=inc_remote;
+						end if;
+					
+					when inc_local =>
+						if sp_control = '1' and rt_control = '1' then
+							state_controller <= ed_local;
+						end if;
+					
+					when inc_remote =>
+						if sp_control = '0' and  rt_control = '0' then
+							state_controller <= set_remote;
+						end if;
+					
+					when set_remote =>
+						if sp_control = '0' and rt_control = '0' then
+							state_controller <= set_remote;
+						end if;
+						
+					when ed_local =>
+						if sp_control = '1'and rt_control = '0' then 
+							state_controller <= ed_local;
+						end if;
+						
+				end case;
+			end if;
+		end process;
 end arq1;
